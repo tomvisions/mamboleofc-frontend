2ba63ecbc078e1108012939f5612e0098ed355a4
Tom <tomc@tomvisions.com>
2022-11-15 00:04:33 -0500

    shared services added

--- src/app/shared.service.ts
+++ src/app/shared.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@angular/core';
-import dotenv from "dotenv";
-dotenv.config();
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +9,10 @@ export class SharedService {
   constructor() {
 
     let options;
-    if (process.env['LOCAL']) {
+    if (process.env['node_dev'] === 'dev') {
       this._apiLocation = 'http://127.0.0.1'
 //      options = {host: '127.0.0.1', dialect: 'mysql', port:3306}
-    } else if ( process.env['NODE_ENV'] === 'staging') {
+    } else if ( process.env['NODE_ENV'] === 'stage') {
       this._apiLocation = 'https://api-stage.mamboleofc.ca/'
     } else {
       this._apiLocation = 'https://api.mamboleofc.ca/';
