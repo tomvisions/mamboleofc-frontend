1b6ff9f46520e78f1ce7e0b89223eac4d775f21c
Tom <tomc@tomvisions.com>
2022-11-16 13:49:34 -0500

    changes

--- src/app/shared.service.ts
+++ src/app/shared.service.ts
@@ -8,10 +8,8 @@ export class SharedService {
   private _apiLocation;
 
   constructor() {
-
-    let options;
     if (environment.node_env === 'dev') {
-      this._apiLocation = 'http://127.0.0.1'
+      this._apiLocation = 'http://127.0.0.1:3000'
     } else if (environment.node_env === 'stage') {
       this._apiLocation = 'https://api-stage.mamboleofc.ca'
     } else {
