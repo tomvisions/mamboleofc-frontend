89580faf0ca87b5853cfecf5ca0b2160ca090a97
Tom <tomc@tomvisions.com>
2022-11-25 10:00:08 -0500

    adding lazy loading

--- src/app/shared.service.ts
+++ src/app/shared.service.ts
@@ -11,7 +11,7 @@ export class SharedService {
     if (environment.node_env === 'dev') {
       this._apiLocation = 'http://127.0.0.1:3000'
     } else if (environment.node_env === 'stage') {
-      this._apiLocation = 'https://api-stage.mamboleofc.ca'
+        this._apiLocation = 'https://api-stage.mamboleofc.ca'
     } else {
       this._apiLocation = 'https://api.mamboleofc.ca';
     }
