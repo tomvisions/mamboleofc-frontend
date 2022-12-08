84349f4068c0610f8174e6db5f19ab861439543b
Tom <tomc@tomvisions.com>
2022-12-08 09:42:35 -0500

    more details

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -40,7 +40,7 @@ export class HomeComponent implements OnInit {
     this._metaTagService.addTags([
       {
         name: 'keywords',
-        content: 'MamboleoFC, MamboleoFC website, soccer training, inclusive community',
+        content: 'MamboleoFC, MamboleoFC website, soccer training, inclusive community, soccer training indoors',
       },
       { name: 'robots', content: 'index, follow' },
       { name: 'author', content: 'Tom Cruickshank' },
